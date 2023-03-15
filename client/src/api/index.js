94c70f12d5ae82a6f079a32ae5513e224bbc3535
Pravesh Kumar <100151644+Pravesh1707@users.noreply.github.com>
2023-03-15 15:06:51 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000'})
+const API = axios.create({ baseURL: 'https://stack-clone-byc3.onrender.com'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
