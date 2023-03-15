e696e9ff1d1a9c35fcbfdb0e65f9fe56f8cf56eb
Pravesh Kumar <100151644+Pravesh1707@users.noreply.github.com>
2023-03-15 18:30:31 +0530

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             }
         }
         dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile'))))
-    },[User?.token, dispatch])
+    },[token, dispatch]);
 
     return (
         <nav className='main-nav'>
