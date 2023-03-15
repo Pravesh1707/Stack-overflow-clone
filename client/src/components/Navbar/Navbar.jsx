26b0e7a7e6de22ceba645e3196fe07706cc80706
Pravesh Kumar <100151644+Pravesh1707@users.noreply.github.com>
2023-03-15 18:43:35 +0530

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect,useCallback } from 'react';
+import React, {useEffect} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import decode from 'jwt-decode'
@@ -14,11 +14,11 @@ const Navbar = () => {
     var User = useSelector((state) => (state.currentUserReducer));
     const navigate = useNavigate();
     
-    const handleLogout =useCallback(() => {
+    const handleLogout =() => {
         dispatch({ type: 'LOGOUT'});
         navigate('/');
         dispatch(setCurrentUser(null));
-    },[handleLogout]);
+    };
     
     useEffect(() => {
         const token = User?.token 
@@ -29,7 +29,7 @@ const Navbar = () => {
             }
         }
         dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile'))))
-    },[User?.token, handleLogout, dispatch]);
+    },[User?.token, dispatch]);
 
     return (
         <nav className='main-nav'>
