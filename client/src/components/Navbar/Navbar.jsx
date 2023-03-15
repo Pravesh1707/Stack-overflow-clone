d6c7a48b53ca6c24d467a8d85bd67ea7cab44e6e
Pravesh Kumar <100151644+Pravesh1707@users.noreply.github.com>
2023-03-15 17:54:35 +0530

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import {React, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import React, {useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import decode from 'jwt-decode'
 
 import logo from '../../assets/logo.png'
@@ -11,21 +11,21 @@ import { setCurrentUser } from '../../actions/currentUser'
 
 const Navbar = () => {
     const dispatch = useDispatch()
-    var User = useSelector((state) => (state.currentUserReducer))
+    var User = useSelector((state) => (state.currentUserReducer));
     const navigate = useNavigate();
     
     const handleLogout = () => {
         dispatch({ type: 'LOGOUT'});
-        navigate('/login');
+        navigate('/');
         dispatch(setCurrentUser(null));
     }
     
     useEffect(() => {
         const token = User?.token 
         if(token){
-            const decodedToken = decode(token)
+            const decodedToken = decode(token);
             if(decodedToken.exp * 1000 < new Date().getTime()){
-                handleLogout()
+                handleLogout();
             }
         }
         dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile'))))
